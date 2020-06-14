ee31321ad4e5f8315dcf3d6d4c93dc5a59eb5b1d
Tom Clarke <tomclarke.work@gmail.com>
2020-06-14 12:43:34 +0100

    Color, counter and reviews done

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,17 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * colors.length);
+};
+
+btn.addEventListener("click", () => {
+    // get random number between 0-3
+    const randomNumber = getRandomNumber();
+    
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
--- 3-reviews/setup/app.js
+++ 3-reviews/setup/app.js
@@ -37,3 +37,62 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+let currentItem = 0;
+
+window.addEventListener("DOMContentLoaded", function() {
+  getReview();
+});
+
+nextBtn.addEventListener("click", function() {
+  // REFACTORED BELLOW TO MAKE CLEANER
+    // if (currentItem === reviews.length-1){
+    //   currentItem = 0;
+    // } else {
+    //   currentItem++;
+    // }
+
+  currentItem++;
+  if(currentItem > reviews.length - 1) currentItem = 0
+
+  getReview();
+});
+
+prevBtn.addEventListener("click", function() {
+  // REFACTORED BELLOW TO MAKE CLEANER
+    // if (currentItem === 0){
+    //   currentItem = reviews.length - 1;
+    // } else {
+    //   currentItem--;
+    // }
+
+
+  currentItem--;
+  if (currentItem < 0 ) currentItem = reviews.length - 1
+
+  getReview();
+});
+
+randomBtn.addEventListener("click", function() {
+  currentItem = Math.floor(Math.random() * reviews.length)
+  getReview();
+});
+
+function getReview (){
+  const review = reviews[currentItem];
+
+  author.textContent = review.name;
+  job.textContent = review.job;
+  info.textContent = review.text;
+  img.src = review.img;
+}
+
