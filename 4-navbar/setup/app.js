021e40884e608d66bc5d6b854bdaea48d932d2b6
Tom Clarke <tomclarke.work@gmail.com>
2020-06-15 15:34:25 +0100

    Navbar, sidebar and modal done

--- 4-navbar/setup/app.js
+++ 4-navbar/setup/app.js
@@ -3,3 +3,21 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+
+const toggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+toggle.addEventListener("click", function() {
+    //REFACTORED BELLOW TO MAKE CLEANER
+    /*
+        const linkClasses = links.classList;
+
+        if(linkClasses.contains("show-links")){
+            linkClasses.remove("show-links");
+        } else {
+            linkClasses.add("show-links");
+        }*/
+
+    links.classList.toggle("show-links");
+});
