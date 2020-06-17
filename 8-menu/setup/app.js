f67d16b1068ecdeb82213ca62c9418f799d3dc59
Tom Clarke <tomclarke.work@gmail.com>
2020-06-17 15:34:35 +0100

    Questions and menu web apps done

--- 8-menu/setup/app.js
+++ 8-menu/setup/app.js
@@ -71,4 +71,79 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 36.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
+
+
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+//const filterBtns = document.querySelectorAll(".filter-btn");
+
+const displayMenuItems = (menuArr) => {
+  let menuItems = menuArr.map(item => {
+    return `<article class="menu-item">
+              <img src=${item.img} class="photo" alt=${item.title}>
+              <div class="item-info">
+                <header>
+                  <h4>${item.title}</h4>
+                  <h4 class="price">${item.price}</h4>
+                </header>
+                <p class="item-text">${item.desc}</p>
+              </div>
+            </article>`;
+  });
+  menuItems = menuItems.join("");
+
+  return menuItems;
+};
+
+function displayFilterButtons  () {
+  const catergories = menu.reduce(function(values, item){
+    
+    if(!values.includes(item.category)){
+      values.push(item.category)
+    }
+    
+    return values;
+  },['all']);
+
+  const catergoryBtns = catergories.map(function(category){
+    return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`
+  }).join("");
+
+  btnContainer.innerHTML =  catergoryBtns
+
+  const filterBtns = document.querySelectorAll(".filter-btn");
+
+  filterBtns.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+      const category = e.currentTarget.dataset.id;
+  
+      const menuCategory = menu.filter(function(menuItem){
+        if(menuItem.category === category) return menuItem;
+      });
+  
+      if(category === "all"){
+        sectionCenter.innerHTML = displayMenuItems(menu);
+      } else {
+        sectionCenter.innerHTML = displayMenuItems(menuCategory);
+      }
+  
+    });
+  });
+
+};
+
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItems(menu);
+  displayFilterButtons();
+});
+
+
