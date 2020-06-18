eec7f5c7050d16035b1868bc26ffdcfeb88bda9e
Tom Clarke <tomclarke.work@gmail.com>
2020-06-18 14:48:58 +0100

    Video, scroll and tabs all done

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -4,10 +4,74 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.getElementById('date')
+date.innerHTML = new Date().getFullYear()
 
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle')
+const linksContainer = document.querySelector('.links-container')
+const links = document.querySelector('.links')
+const navbar = document.getElementById('nav')
+const topLink = document.querySelector('.top-link')
+const scrollLinks = document.querySelectorAll('.scroll-link')
+
+navToggle.addEventListener('click', function () {
+  //linksContainer.classList.toggle("show-links");
+
+  const containerHeight = linksContainer.getBoundingClientRect().height
+  const linksHeight = links.getBoundingClientRect().height
+
+  containerHeight === 0
+    ? (linksContainer.style.height = `${linksHeight}px`)
+    : (linksContainer.style.height = 0)
+})
 
 // ********** fixed navbar ************
+window.addEventListener('scroll', function () {
+  const scrollHeight = window.pageYOffset
+  const navHeight = navbar.getBoundingClientRect().height
+
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav')
+  } else {
+    navbar.classList.remove('fixed-nav')
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add('show-link')
+  } else {
+    topLink.classList.remove('show-link')
+  }
+})
 
 // ********** smooth scroll ************
 // select links
+scrollLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const id = e.currentTarget.getAttribute('href').slice(1)
+    const element = document.getElementById(id)
+
+    const navHeight = navbar.getBoundingClientRect().height
+    const containerHeight = linksContainer.getBoundingClientRect().height
+    const fixedNav = navbar.classList.contains('fixed-nav')
+
+    let position = element.offsetTop - navHeight
+
+    if (!fixedNav) {
+      position = position - navHeight
+    }
+
+    if (navHeight > 82) {
+      position = position + containerHeight
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    })
+
+    linksContainer.style.height = 0
+  })
+})
---
+++ 11-tabs/setup/app.js
@@ -0,0 +1,20 @@
+const btns = document.querySelectorAll('.tab-btn')
+const about = document.querySelector('.about')
+const articles = document.querySelectorAll('.content')
+
+about.addEventListener('click', function (e) {
+  const id = e.target.dataset.id
+
+  if (id) {
+    btns.forEach(function (btn) {
+      btn.classList.remove('active')
+    })
+
+    articles.forEach(function (article) {
+      article.classList.remove('active')
+    })
+
+    e.target.classList.add('active')
+    document.getElementById(id).classList.add('active')
+  }
+})
