5f194800b2b3ebc24a44e9e9e6ddc47cc72a0a1c
Tom Clarke <tomclarke.work@gmail.com>
2020-06-19 15:18:49 +0100

    Update app.js

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -25,3 +25,73 @@ const weekdays = [
 const giveaway = document.querySelector('.giveaway')
 const deadline = document.querySelector('.deadline')
 const items = document.querySelectorAll('.deadline-format h4')
+
+let futureDate = new Date(2020, 5, 19, 14, 30, 0, 0)
+
+const year = futureDate.getFullYear()
+let hours = futureDate.getHours()
+const minutes = futureDate.getMinutes()
+const month = months[futureDate.getMonth()]
+const day = weekdays[futureDate.getDay()]
+const date = ordinal_suffix_of(futureDate.getDay())
+let dayPart = 'am'
+
+if (hours > 12) dayPart = 'pm'
+
+if (hours.toString().length === 1) hours = '0' + hours
+
+giveaway.textContent = `giveaway ends on ${day} ${date} ${month} ${year} ${hours}:${minutes}${dayPart}`
+
+const futureTime = futureDate.getTime()
+
+function getRemainingTime() {
+  const today = new Date().getTime()
+  const t = futureTime - today
+
+  const oneDay = 24 * 60 * 60 * 1000
+  const oneHour = 60 * 60 * 1000
+  const oneMinute = 60 * 1000
+
+  let days = Math.floor(t / oneDay)
+  let hours = Math.floor((t % oneDay) / oneHour)
+  let minutes = Math.floor((t % oneHour) / oneMinute)
+  let seconds = Math.floor((t % oneMinute) / 1000)
+
+  const values = [days, hours, minutes, seconds]
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`)
+    } else {
+      return item
+    }
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index])
+  })
+
+  if (t < 0) {
+    clearInterval(countdown)
+    deadline.innerHTML = `<h4 class="expired">Sorry, this giveaway has ended</h4>`
+  }
+}
+
+let countdown = setInterval(getRemainingTime, 1000)
+
+getRemainingTime()
+
+function ordinal_suffix_of(i) {
+  const j = i % 10,
+    k = i % 100
+  if (j == 1 && k != 11) {
+    return i + 'st'
+  }
+  if (j == 2 && k != 12) {
+    return i + 'nd'
+  }
+  if (j == 3 && k != 13) {
+    return i + 'rd'
+  }
+  return i + 'th'
+}
